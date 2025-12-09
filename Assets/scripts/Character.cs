ee3e69bf9d939ec1559a0f9947d9e679ac0229d1
Evan Pfeil <evpfeil@davidson.edu>
2025-12-09 16:16:04 -0500

    new commit

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -1,6 +1,45 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class Character
 {
-    
+    public string name;
+    public int level;
+
+    public Character(string name, int level)
+    {
+        this.name = name;
+        this.level = level;
+    }
+
+    public virtual void PrintStatsInfo()
+    {
+        Debug.Log($"Character: {name}, Level: {level}");
+    }
+}
+
+public struct Weapon
+{
+    public string weaponName;
+    public int damage;
+
+    public Weapon(string weaponName, int damage)
+    {
+        this.weaponName = weaponName;
+        this.damage = damage;
+    }
+}
+
+public class Paladin : Character
+{
+    public Weapon weapon;
+
+    public Paladin(string name, int level, Weapon weapon) : base(name, level)
+    {
+        this.weapon = weapon;
+    }
+
+    public override void PrintStatsInfo()
+    {
+        Debug.Log($"Paladin: {name}, Level: {level}, Weapon: {weapon.weaponName} ({weapon.damage} dmg)");
+    }
 }
